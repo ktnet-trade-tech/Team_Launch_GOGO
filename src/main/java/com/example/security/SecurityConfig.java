67d5d38421f60bcaa75f4609eff54a1a65e71774
youdongkwan <dkyou7@naver.com>
2020-07-06 15:11:29 +0900

    project init

---
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.security;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity  // 이거 안해줘서 그동안 계속 안됬었음
+@Slf4j
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String password = passwordEncoder().encode("123");
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password(password).roles("USER")
+            .and()
+                .withUser("manager").password(password).roles("USER", "MANAGER")
+            .and()
+                .withUser("admin").password(password).roles("USER", "MANAGER", "ADMIN");
+    }
+    @Bean
+    // BCryptPasswordEncoder는 Spring Security에서 제공하는 비밀번호 암호화 객체입니다.
+    // Service에서 비밀번호를 암호화할 수 있도록 Bean으로 등록합니다.
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    // js, css, image 설정은 보안 설정의 영향 밖에 있도록 만들어주는 설정.
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().permitAll();
+    }
+}
